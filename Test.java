611bd67d475fb9b69d90c3e065504c6a003c92af
Valorias <abdullahbinladin98@gmail.com>
2017-02-20 12:07:22 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,29 +1,44 @@
+import java.io.*;
 public class Test {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException{
     
     Database csStudents = new Database("AP CS Students", new Column("Name", "String"), new Column("Grade", "int"), new Column("e-mail", "String"));
     Database myFamilyMembers = new Database ("Abdullah's Family", new Column("Name", "String"), new Column("Relation", "String"));
-    Database untitledTest = new Database();
-    Database untitledTest1 = new Database();
+    Database accounts  = new Database("Accounts", new Column("UserName", "String"), new Column("Password", "String"));
     
-    System.out.println(untitledTest.getName());
-    System.out.println(untitledTest1.getName());
     
-    csStudents.addEntry(new Entry("Abdullah", 12, "AbdullahBinladin17@kingsacademy.edu.jo"));
-    csStudents.addEntry(new Entry("Karam", 11, "KaramImmjalli18@kingsacademy.edu.jo"));
-    csStudents.addEntry(new Entry("Tariq", 12, "TariqSerhan17@kingsacademy.edu.jo"));
+    accounts.addEntry(new Entry("CoolDude12", "cooldude"));
+    accounts.addEntry(new Entry("BestCodPlayer", "123456789"));
+    accounts.addEntry(new Entry("FastCat1337", "hatsandshoes"));
+    
+    System.out.println(accounts);
+    try {
+      csStudents.addEntry(new Entry("Abdullah", 12, "AbdullahBinladin17@kingsacademy.edu.jo"));
+      csStudents.addEntry(new Entry("Karam", 11, "KaramImmjalli18@kingsacademy.edu.jo"));
+      csStudents.addEntry(new Entry("Tariq", 12, "TariqSerhan17@kingsacademy.edu.jo"));
+      csStudents.addEntry(new Entry("entries/Amr.txt"));
+    } catch (IOException e){
+      e.printStackTrace();
+    }
     
     System.out.println(csStudents);
     
-    myFamilyMembers.addEntry(new Entry("Ibrahim", "Father"));
-    myFamilyMembers.addEntry(new Entry("Muneera", "Mother"));
-    myFamilyMembers.addEntry(new Entry("Lama", "Cousin"));
-    myFamilyMembers.addEntry(new Entry("Ahmad", "Uncle"), new Entry("Yeslam", "Uncle"));
+    try {
+      myFamilyMembers.addEntry(new Entry("Ibrahim", "Father"),
+                               new Entry("Muneera", "Mother"),
+                               new Entry("Ahmad", "Cousin"),
+                               new Entry("Mohammad","Uncle"),
+                              );
+    }catch(IOException e){
+      System.out.println("File not found");
+      e.printStackTrace();
+    }
     
     System.out.println(myFamilyMembers);
     
     csStudents.outputToFile();
     myFamilyMembers.outputToFile();
+    accounts.outputToFile();
   
   }
 }
